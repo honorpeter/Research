7f91bd974a9bc4b5ff3a73f7706cafde398c658a
Jared Bold <jmb4621@rit.edu>
2014-09-05 11:00:48 -0400

    Cleaned up deleted files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Owner:  Jared Bold
-# Purpose:
-The purpose of this repository is to store all of my work for my Graduate Research
--- Zynq_Kernel/scripts/build_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./build_fsbl.sh
-./build_u-boot.sh
-./build_root-fs.sh /home/jared/Research/Zynq_SD_Images/2013.4/uramdisk.image.gz
-./build_dtb.sh /home/jared/Research/Zynq_SD_Images/2013.4/zc70x/zc702/devicetree.dtb
-./build_linux-kernel.sh
-./build_boot-image.sh
